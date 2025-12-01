83db8a29af20b543a3fd961464cf2ac366fc70da
Hossein Tizro Mikal <hosseintizro2006@gmail.com>
2025-12-01 03:47:44 +0330

    Create Prime number.py

---
+++ Prime number.py
@@ -0,0 +1,13 @@
+from math import sqrt
+counter = 0
+number = 2
+while counter < 100:
+  isPrime = True
+  for i in range(2, int(sqrt(number))+1):
+    if number%i == 0:
+      isPrime = False
+      break
+      if isPrime:
+        counter += 1
+        print(number)
+      number += 1
